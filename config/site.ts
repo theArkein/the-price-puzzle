311b867fc76ea1071c1d8c8c5cfee200791eb738
Sarad Singh <theArkein.dev@gmail.com>
2024-05-01 22:19:42 +0545

    update page title, description

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "The Price Puzzle",
+  description: "Solving the price mystry",
   mainNav: [
     {
       title: "Home",
